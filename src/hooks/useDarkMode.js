34c9a41630b03f05aec2567b2fb5173192ac35d0
newbi462 <isthisemailfree@gmail.com>
2019-12-04 16:02:23 -0500

    use the effect hook. -Import and set up your effect hook.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 
 import { useLocalStorage } from "/useLocalStorage";
 
+useEffect(() => {
+  //
+},[])
+
 export const useDarkMode = () => {
   const [someValue, setSomeValue] = useLocalStorage("myKeyIsLSNeedsATeacherDay");
-  
+
 };
