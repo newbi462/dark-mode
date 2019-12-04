d9622bba5a68f27f67b6fbef0aedf39c022a830b
newbi462 <isthisemailfree@gmail.com>
2019-12-04 15:12:15 -0500

    In the callback function, we'll check to see if the item we passed in already exists in localStorage, and return that value, otherwise we'll return whatever initialValue was passed in.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,23 @@
 import { useState } from "react";
 
 export const useLocalStorage = () =>
-  const [storedValue, setStoredValue] = useState();
+  const [storedValue, setStoredValue] = useState(() => {
+    // Get from local storage by key
+    const item = window.localStorage.getItem(key);
+    // Parse and return stored json or, if undefined, return initialValue
+    return item ? JSON.parse(item) : initialValue;
+  });
 };
+// To retrieve an item from localStorage, call localStorage.getItem('itemName')
+// If that item doesn't exist, it will return undefined
+
+
+
+/*
+Quick note, if you pass in arrays or objects to localStorage,
+you will need to parse it into JSON.
+
+Then when you retrieve it, like we do below,
+you'll need to parse it back into regular JavaScript
+
+*/
